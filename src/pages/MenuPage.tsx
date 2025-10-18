1cfe537342b2b19ae227fc23983c7f7ca6428afb
[dyad] <mohammadfakkar@yahoo.com>
2025-10-18 23:09:53 +0330

    [dyad] تنظیم موقعیت آیکون جستجو - wrote 1 file(s)

--- src/pages/MenuPage.tsx
+++ src/pages/MenuPage.tsx
@@ -105,9 +105,9 @@ const MenuPage: React.FC = () => {
             placeholder={t('search_menu_placeholder')}
             value={searchTerm}
             onChange={(e) => setSearchTerm(e.target.value)}
-            className="bg-white/20 text-white placeholder:text-gray-300 border-none rounded-full pl-10"
+            className="bg-white/20 text-white placeholder:text-gray-300 border-none rounded-full pl-12"
           />
-          <Search className="absolute left-3 top-1/2 -translate-y-1/2 h-5 w-5 text-gray-300" />
+          <Search className="absolute left-4 top-1/2 -translate-y-1/2 h-5 w-5 text-gray-300" />
         </div>
 
         {categories.length === 0 ? (
